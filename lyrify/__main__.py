036221870821738f367e53da25795ce39c21b524
Shameek Ray <shameek@umich.edu>
2019-05-13 22:50:52 -0400

    Genius API refactor

--- lyrify/__main__.py
+++ lyrify/__main__.py
@@ -8,24 +8,28 @@ import requests
 import sys
 
 class SpotifyAPI():
+    album = None
+    artist = None
+    song = None
+
     def __init__(self):
-        _ = 0
+        self.song = "Street lights"
+        self.artist = "Kanye West"
 
 class GeniusAPI():
-    api_base_url = 'https://api.genius.com/'
-    headers = { 
-        'Authorization': 'Bearer ' + CLIENT_ACCESS_TOKEN 
-        }
-    def __init__(self):
-        _ = 0
+    authority = 'https://api.genius.com/'
 
     @classmethod
     def get_song_url(self, artist, song, album = None):
-        query = artist + ' ' + song
-        search_endpoint = self.api_base_url + 'search?q=' + query + '&access_token=' + CLIENT_ACCESS_TOKEN
-        response = requests.get(search_endpoint)
-        if response.json()['meta']['status'] == 200 and len(response.json()["response"]["hits"]) > 0:
-            return (response.json()["response"]["hits"][0]['result']['url'])
+        query = ''.join([artist, ' ', song])
+        if album:
+            query += album
+        
+        search_endpoint = ''.join([self.authority, 'search?q=', query, '&access_token=', CLIENT_ACCESS_TOKEN])
+        response = requests.get(search_endpoint).json()
+
+        if response['meta']['status'] == 200 and len(response["response"]["hits"]) > 0:
+            return (response["response"]["hits"][0]['result']['url'])
         else:
             return None
 
@@ -35,11 +39,11 @@ class GeniusAPI():
         html = BeautifulSoup(song.text, 'html.parser')
         lyrics = html.find('div', class_='lyrics').get_text()
         return lyrics
-        
 
 def main(args=None):
+    spotify = SpotifyAPI()
     genius = GeniusAPI()
-    print (genius.get_song_url("Kanye West", "Street lights"))
+    print (genius.get_song_url(spotify.artist, spotify.song))
     # print (genius.scrap_song_url('https://genius.com/Drake-jungle-lyrics'))
 
 if __name__ == "__main__":
